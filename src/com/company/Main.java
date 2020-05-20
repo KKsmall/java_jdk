125f4463eca4af10ea865fa503f752688af1088d
liujin <389203946@qq.com>
2020-05-20 10:33:02 +0800

    d

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author liujin
+ * @datetime 2020/5/20 9:58
+ */
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("1", 2);
+        System.out.println(map);
+    }
+}
